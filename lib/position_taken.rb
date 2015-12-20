0f6c4e3d58703891e3830d3c2d43b93e0fdf28f3
sophiew7 <shmwhite@gmail.com>
2015-12-20 18:08:08 +0000

    nitrous-backup at Sun Dec 20 18:08:08 UTC 2015

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
-# code your #position_taken? method here!
+def position_taken?(board,position)
+  if board[position]== " " || "" || nil
+    return false
+  elsif board[position]=="X" || "O"
+    return true
+  else
+    return nil
+  end
+end
+
